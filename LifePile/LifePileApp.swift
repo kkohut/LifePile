d24a554285f54587c7415fe94b67149c4744d86f
Kevin Kohut <kevin-kohut@online.de>
2023-04-29 16:22:21 +0200

    Initial Commit

---
+++ LifePile/LifePileApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifePileApp.swift
+//  LifePile
+//
+//  Created by Kevin Kohut on 29.04.23.
+//
+
+import SwiftUI
+
+@main
+struct LifePileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
