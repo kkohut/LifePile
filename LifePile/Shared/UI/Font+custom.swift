b9779bb20534cebfb25c3d7d82541be4b2d9e87a
Kevin Kohut <kevin-kohut@online.de>
2023-06-04 03:54:17 +0200

    #7: Clean up

--- LifePile/Shared/UI/Font+custom.swift
+++ LifePile/Shared/UI/Font+custom.swift
@@ -9,8 +9,8 @@ import SwiftUI
 import Foundation
 
 extension Font {
-    private static let customFont = "GillSans"
     static let largeTitle = Font.custom(customFont, size: 34, relativeTo: .largeTitle)
     static let headline = Font.custom(customFont, size: 17, relativeTo: .headline)
     static let body = Font.custom(customFont, size: 17, relativeTo: .body)
+    private static let customFont = "GillSans"
 }
